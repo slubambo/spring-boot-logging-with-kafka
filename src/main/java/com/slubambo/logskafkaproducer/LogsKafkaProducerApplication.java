843658f538aa1f7c8acb4c3ac8bc5c56e02dc1d1
slubambo <slubambo57@gmail.com>
2023-08-30 16:38:37 +0300

    🚧 Project Initialization

---
+++ src/main/java/com/slubambo/logskafkaproducer/LogsKafkaProducerApplication.java
@@ -0,0 +1,13 @@
+package com.slubambo.logskafkaproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogsKafkaProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogsKafkaProducerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/slubambo/logskafkaproducer/LogsKafkaProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slubambo.logskafkaproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogsKafkaProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
